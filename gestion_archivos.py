03808801d8efe251e05972b589d5afeccadb2983
VictorANT200 <20A3110452@untels.edu.pe>
2024-06-23 20:16:08 -0500

    commit para prueba

---
+++ gestion_archivos.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+ 
+def crear_archivo(nombre, contenido):
+    archivo = open(nombre,"wt")
+    archivo.write(contenido)
+    archivo.close()
+ 
+def eliminar_archivo(nombre):
+    os.remove(nombre)
+ 
+def agregar_contenido_archivo(nombre, contenido):
+    archivo = open(nombre,"at")
+    archivo.write("\n" + contenido)
+    archivo.close()
+ 
+def leer_archivo(nombre):
+    archivo = open(nombre,"rt",encoding='utf8')
+    contenido = archivo.read()
+    return contenido
+
+
